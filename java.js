17e176edea139fb8915dbaacbe574153c3b0b86c
Caroline Cran <cranch.caroline@gmail.com>
2019-07-21 18:59:00 -0400

    removed about.html page - displaying as new div on home.html page with similar toggle view as contact form

--- java.js
+++ java.js
@@ -3,6 +3,10 @@ $("#contact").click(function(){
     console.log("contact form opened");
   });
 
+  $("#about").click(function(){
+    $("#aboutMe").toggle();
+    console.log("about me opened");
+  });
 
 
 $("#submit").click(function(){
