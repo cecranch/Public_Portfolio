b8769efafece30b3d52c76ecc54448b2bb9485e3
Caroline Cran <cranch.caroline@gmail.com>
2019-07-21 13:47:15 -0400

    added resume icon to nav bar- respaced nav bar

--- java.js
+++ java.js
@@ -2,3 +2,15 @@ $("#contact").click(function(){
     $("#contactForm").toggle();
     console.log("contact form opened");
   });
+
+
+
+$("#submit").click(function(){
+
+  Swal.fire({
+    type: 'success',
+    title: 'Thanks!',
+    text: 'Look forward to chatting with you soon!',
+    timer: 5000
+  })
+});
